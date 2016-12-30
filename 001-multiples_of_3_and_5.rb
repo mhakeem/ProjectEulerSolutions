81c2b20999446487b32ec01c6d5205401a199024
M. Hakeem <mhakeem@users.noreply.github.com>
2016-12-29 23:18:17 -0700

    Update 001-multiples_of_3_and_5.rb

--- 001-multiples_of_3_and_5.rb
+++ 001-multiples_of_3_and_5.rb
@@ -1,10 +1,9 @@
 # https://projecteuler.net/problem=1
 
 multiple_of_3_or_5 = -> (i) { i if i%3==0 || i%5==0 }
-puts 1000.times.map(&multiple_of_3_or_5)
-               .compact
+puts 1000.times.select(&multiple_of_3_or_5)
                .inject(:+)
 
 # common version (non-functional)
-# total = 0
-# puts 1000.times { |i| total += i if i%3==0 || i%5==0 }
+total = 0
+puts 1000.times { |i| total += i if i%3==0 || i%5==0 }
